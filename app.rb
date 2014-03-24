52d94e90f58ac2df2d33470deb63708cc0601d71
Evan Tedesco <Evantedesco@gmail.com>
2014-03-24 15:09:42 -0600

    Allow user to create new item displayed on homepage

--- app.rb
+++ app.rb
@@ -2,11 +2,17 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  ITEMS_LIST = []
   get '/' do
     erb :index
   end
 
   get '/items/new' do
+    erb :new_items
+  end
+
+  post '/' do
+   ITEMS_LIST << params[:new_item]
     erb :index
   end
 
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -8,11 +8,9 @@ feature "managing list of items" do
   scenario "Displays current list of items" do
   visit "/"
     click_link "Create Item"
-    fill_in "new_item", :with => "Chana Masala"
-
-
-
+    fill_in "new_item", :with => "Channa Masala"
+    click_on "Create New Item"
+    expect(page).to have_content("Channa Masala")
 
   end
-
 end
