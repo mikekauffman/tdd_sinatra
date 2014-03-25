5f0bd69978fe7e2d815d4d99a7900e85d0637606
Evan Tedesco <Evantedesco@gmail.com>
2014-03-25 14:17:54 -0600

    Fixed bug so items would not display on refresh of page

--- app.rb
+++ app.rb
@@ -2,11 +2,12 @@ require 'sinatra/base'
 require './lib/item_class'
 require './lib/item_list'
 
+ARRAY = ItemsList.new
+
 class App < Sinatra::Base
 
-  set :array, ItemsList.new
-
   get '/' do
+    @display_list = ARRAY.items_list
     erb :index
   end
 
@@ -15,12 +16,13 @@ class App < Sinatra::Base
   end
 
   post '/' do
-    @new_item = params[:new_item]
-    erb :index
+    @display_list = ARRAY.create_item(params[:new_item])
+    redirect '/'
   end
 
   get '/single_item' do
     erb :item
   end
+
 
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,3 @@
 require "./app"
-ARRAY = ItemsList.new
+
 run App
